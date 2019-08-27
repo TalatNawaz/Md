54bb8ad4d8e224e07b7ee356b71db5832987fcff
talatnawaz <mtnaz786@gmail.com>
2019-08-27 14:21:48 +0500

    first 4

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 
 app.use(express.static(path.resolve(__dirname, "../dist")))
 
-app.listen(process.env.PORT || 200, function() {
+app.listen(process.env.PORT ||3000 , function() {
     console.log(
       "Express server listening on port %d in %s mode",
       this.address().port,
